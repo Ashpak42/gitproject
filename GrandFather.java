bdc7dc2150e6da2166e46be9da46a76d14085353
Ashpak Shaikh <ashpaks199@gmail.com>
2023-03-14 21:21:48 +0530

    One Testcases added to GrandFather

---
+++ GrandFather.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+public class GrandFather {
+
+	String name;
+	String surName;
+	int age;
+	int home;
+	public void GrandFather()
+	{
+		String name="ashpak";
+		String surName="Shaikh";
+		int age=24;
+		int home=1;
+		System.out.println(name +"  " +surName + " " + " " +age + " " + home   );
+		
+		
+	}
+	
+}
