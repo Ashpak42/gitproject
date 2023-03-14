b677fc7071dab073b751ff1ad2da49efa5ffdaea
Ashpak Shaikh <ashpak.shaikh@resilinc.com>
2023-03-14 21:06:23 +0530

    One Test Case for Father is Ready.

---
+++ Father.java
@@ -0,0 +1,16 @@
+package inheritance;
+
+public class Father extends GrandFather {
+	
+	String clg= "vit";
+	
+	
+
+	public static void main(String[] args) {
+		
+		Father f1=new Father();
+		GrandFather g1=new GrandFather();
+		f1.GrandFather();
+		
+	}
+}
